e42fe4603a83b87e4a03e78ffb6da4daf3c531e7
Chen Cohen <cohencil@gmail.com>
2020-01-29 18:17:50 +0200

    Initial Commit

---
+++ src/main/java/io/jenkins/plugins/sample/ParallelPhase.java
@@ -0,0 +1,51 @@
+package io.jenkins.plugins.sample;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import jenkins.tasks.SimpleBuildStep;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+public class ParallelPhase extends Builder implements SimpleBuildStep {
+
+    private String name;
+
+    @DataBoundConstructor
+    public ParallelPhase(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath filePath, @Nonnull Launcher launcher, @Nonnull TaskListener taskListener) throws InterruptedException, IOException {
+        taskListener.getLogger().println("Hello, " + name + "!");
+    }
+
+    @Symbol("parallel")
+    @Extension
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "parallelPhase";
+        }
+
+    }
+}
